883d4d5e721fbf703f4af98e6febbc0af2f38522
vuongsythanh <vuongsythanh.tk@gmail.com>
2019-05-28 22:23:00 +0700

    Updates

---
+++ precache-manifest.3428ccbcb72b3c09773d04576bada85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7361096392fad940cb72ee53655e80b2",
+    "url": "/benri/index.html"
+  },
+  {
+    "revision": "1f7df901cacc0d8fdaca",
+    "url": "/benri/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/benri/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1f7df901cacc0d8fdaca",
+    "url": "/benri/static/js/main.59ff9a7f.chunk.js"
+  },
+  {
+    "revision": "85572882c8cabd46190c",
+    "url": "/benri/static/js/runtime~main.7d91aa23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/benri/static/media/logo.5d5d9eef.svg"
+  }
+]);

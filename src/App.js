749e8d2ae535fe021d3d6cfeadff73010631db3b
vuongsythanh <vuongsythanh.tk@gmail.com>
2019-05-30 02:12:33 +0700

    updata

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect} from 'react';
 import firebase from './firebase/firebaseConfig';
-import { Table, Container, Row, Col, Jumbotron, Button, InputGroup, FormControl } from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron } from 'react-bootstrap';
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider, { Search, ColumnToggle } from 'react-bootstrap-table2-toolkit';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 function App() {
 
   const [data, setData] = useState([]);
-  const inputText = useRef(null);
 
   const fetchData = async () => {
     const result = await firebase.database().ref().child("words");
@@ -19,80 +21,100 @@ function App() {
     fetchData();
   }, []);
 
-  const onChangeText = () => {
-    let text = inputText.current.value.toLowerCase();
-    if(text!==''){
-      let newData = data.filter(item => item.vn.toLowerCase().includes(text));
-      setTimeout(() => {
-        setData(newData);
-      }, 2000);
-    } else {
-      setTimeout(() => {
-        fetchData();
-      }, 2200);
-    }
-  }
+  const { SearchBar } = Search;
+  const { ToggleList } = ColumnToggle;
+
+  const columns = [{
+    text: 'Từ vựng',
+    dataField: 'jp',
+    headerClasses: 'demo-row-odd'
+  }, {
+    text: 'Âm hán việt',
+    dataField: 'vn',
+  }, {
+    text: 'Cách đọc',
+    dataField: 'read'
+  }, {
+    text: 'Ý nghĩa',
+    dataField: 'meaning'
+  }, {
+    text: 'Cách nhớ',
+    dataField: 'note'
+  }, {
+    text: 'Trình độ',
+    dataField: 'level'
+  }, {
+    text: 'Loại từ',
+    dataField: 'rank'
+  }];
+
+  const options = {
+    paginationSize: 4,
+    pageStartIndex: 1,
+    firstPageText: 'Đầu',
+    prePageText: '<',
+    nextPageText: '>',
+    lastPageText: 'Cuối',
+    sizePerPageList: [{
+      text: '5', value: 5
+    }, {
+      text: '10', value: 10
+    }]
+  };
 
   return (
     <Container className="mt-4">
       <Row>
         <Col>
-        <Jumbotron>
+        <Jumbotron className="shadow-sm p-4 mb-4 bg-white rounded">
           <h1>Chào bạn!</h1>
-          <p>
-            Nếu bạn đang gặp khó khăn trong việc loay hoay tra nghĩa từ vựng hán việt 
-            thì đây là tiện ích bạn cần!
-          </p>
-          <p>
-            <Button variant="primary">Xem về mình</Button>
-          </p>
+          <br/>
+          <div className="alert alert-primary">
+            <a href="null" className="alert-link">Nếu bạn đang loay hoay tra nghĩa từ vựng hán việt, 
+            thì đây là tiện ích bạn cần!</a>.
+          </div>
         </Jumbotron>
         </Col>
       </Row>
-      <Row>
-        <Col>
-        <InputGroup className="mb-3">
-          <FormControl
-            placeholder="Nhập từ hán việt..."
-            aria-label="Nhập từ hán việt..."
-            aria-describedby="basic-addon2"
-            ref={inputText}
-            onChange={onChangeText}
-          />
-          <InputGroup.Append>
-            <InputGroup.Text id="basic-addon2">Tìm kiếm</InputGroup.Text>
-          </InputGroup.Append>
-        </InputGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Table striped bordered hover responsive="sm" variant="dark">
-            <thead>
-              <tr>
-                  <th>Từ vựng</th>
-                  <th>Âm hán việt</th>
-                  <th>Cách đọc</th>
-                  <th>Ý nghĩa</th>
-                  <th>Cách nhớ</th>
-                  <th>Trình độ</th>
-                  <th>Loại từ</th>
-                </tr>
-            </thead>
-            <tbody>
-              {data.map(item => (
-                  <tr key={item.jp}>
-                    <td>{ item.jp }</td>
-                    <td>{ item.vn }</td>
-                    <td>{ item.read }</td>
-                    <td>{ item.meaning }</td>
-                    <td>{ item.note }</td>
-                    <td>{ item.level }</td>
-                    <td>{ item.rank }</td>
-                  </tr>
-              ))}
-            </tbody>
-          </Table>
+      <Row className="mb-5">
+        <Col className="mb-5">
+          <ToolkitProvider
+            keyField="jp"
+            data={ data }
+            columns={ columns }
+            search
+            columnToggle
+          >
+            {
+              props => (
+                <div>
+                  <p className="text-muted font-weight-light">(*) Ấn vào tên cột để ẩn bớt cột <em>(hiển thị tốt hơn đối với các thiết bị di động).</em></p>
+                  <ToggleList 
+                    { ...props.columnToggleProps } 
+                    className="customToggleList"
+                  />
+                  
+                  <br/>
+                  <hr className="my-2" />
+                  <br/>
+                  <SearchBar 
+                    { ...props.searchProps } 
+                    className="custome-search-field mb-4"
+                    placeholder="Nhập từ cần tìm..."
+                    style={{padding:'1.5rem', fontWeight: 'bold'}}
+                  />
+                  <BootstrapTable
+                    responsive
+                    striped
+                    hover
+                    { ...props.baseProps }
+                    pagination={ paginationFactory(options) }
+                    noDataIndication="Lấy dữ liệu..."
+                  />
+                </div>
+              )
+            }
+          </ToolkitProvider>
         </Col>
       </Row>
     </Container>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 ReactDOM.render( < App / > , document.getElementById('root'));
 

82b454a0dd54a5cec0cb49bb44678aafe666cc7c
vuongsythanh <vuongsythanh.tk@gmail.com>
2019-06-01 18:28:16 +0700

    Updates

---
+++ precache-manifest.206ac18cbc8af70a81aa9cb0a9c18a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70eb6ae268e6aa018821863686066ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8435551cf2b3e04ea247",
+    "url": "/static/css/2.5121d26c.chunk.css"
+  },
+  {
+    "revision": "444cb4e7d04d0b27fe5b",
+    "url": "/static/css/main.a18cdbb7.chunk.css"
+  },
+  {
+    "revision": "8435551cf2b3e04ea247",
+    "url": "/static/js/2.d401e7e0.chunk.js"
+  },
+  {
+    "revision": "444cb4e7d04d0b27fe5b",
+    "url": "/static/js/main.1805c22d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
